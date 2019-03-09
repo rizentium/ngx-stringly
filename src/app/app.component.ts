39bd9ee66689694fe36c301a63eaa9884a532e83
rizentium <rizentium@gmail.com>
2019-03-09 20:50:35 +0700

    Updated: demo apps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgxStringlyService } from '../../projects/ngx-stringly/src/lib/ngx-stringly.service';
+import {StringlyInterface} from '../../projects/ngx-stringly/src/interfaces/stringly-interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-stringly';
+  title = 'ngx-stringly example';
+  text = 'The quick brown fox jumps over the lazy dog';
+  find: string;
+  result: StringlyInterface;
+
+  constructor(public stringly: NgxStringlyService) {
+
+  }
+
+  onChanged() {
+    this.result = this.stringly.parsing(this.text, this.find, false);
+  }
 }
